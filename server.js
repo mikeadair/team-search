65572b764f69d483e1142b883ef91deaef5393f8
Chip <bachippi@svsu.edu>
2016-11-28 18:28:08 +0000

    mike's additions

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ LolApi.init('afadd45a-7661-4905-9b78-d66ea2493f8a', 'na');
 const reqSec = 10;
 const reqMin = 500;
 
-const timeout = 1000;
+//const timeout = 1000;
 
 LolApi.setRateLimit(reqSec, reqMin);
 
@@ -21,7 +21,7 @@ function getSummonerID(s, callback){
         var id = summoner[s]["id"];
         callback(id);
     }, function(err){
-        callback("Not Found");
+        callback({"error":"Summoner not Found."});
     });
 }
 
@@ -76,17 +76,63 @@ function accountVerify(id, token, callback){
     });
 }
 
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
 
+function genString(char){
+    var str = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < char; i++ )
+        str += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return str;
+}
+
+app.get('/genString', function(req, res) {
+    var email = req.query.email;
+    var summoner = req.query.summoner;
+    var string = genString(6);
+    
+    //Make sure queries are sent
+    if(!email || !summoner){
+        res.send({"error": "Missing 'email' or 'summoner'."});
+        return;
+    }
+    
+    //Make sure email is valid
+    if(!validateEmail(email)){
+        res.send({"error": "Invalid 'email' address."});
+        return;
+    }
+    
+    //Check req.query.email against DB to see if already in use.
+    var str = genString(6);
+    getSummonerID(summoner, function(data){
+        var uid = data;
+        if(uid['error']){
+            res.send(uid);
+            return;
+        }
+        res.send({"string": str, "email": email, "summoner": summoner,"id": uid});    
+    });
+    //Maybe here we should return the email applied with, string, and account wanted to link.
+    //Next step will just be a button that sends all of this info back to us then its checked
+    //against accountVerify and added to the DB if success.
+});
 
 app.get('/test', function (req, res){
-    var user = req.query.summoner;
+    var summoner = req.query.summoner;
     var teamMate = req.query.teamMate;
-    console.log("User: "+user);
+    console.log("User: "+summoner);
     console.log("TeamMate: "+teamMate);
-    getSummonerID(user, function(data){
+    getSummonerID(summoner, function(data){
       var id = data;
-      if(id == "Not Found"){
-          res.send("Player Not Found");
+      if(id['error']){
+          res.send(id);
+          return;
       }
       console.log("User-ID: "+id);
       getTopChamps(id, function(data){
